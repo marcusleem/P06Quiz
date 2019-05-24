83184f77c0520e088075869da0035257879bbbf2
18037942 <18037942@rp.edu.sg>
2019-05-24 16:16:43 +0800

    Submission

--- app/src/main/java/sg/edu/rp/soi/p06_quiz/MainActivity.java
+++ app/src/main/java/sg/edu/rp/soi/p06_quiz/MainActivity.java
@@ -2,8 +2,16 @@ package sg.edu.rp.soi.p06_quiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvOperation;
+    int mode = 0;
 
 
 
@@ -11,7 +19,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvOperation = findViewById(R.id.tvOperation);
+
+
+        registerForContextMenu(tvOperation);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        if(v.getId() == R.id.plus){
+        mode = 1;
+        }else{
+        mode = 2;
+        }
+
+        Log.d("TEST", "" + v.getId() + " " + mode);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        if()
     }
 
 
-}
+    }
+
+
